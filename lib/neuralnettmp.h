5e7b2bab4b7a7adb1c4d9c2db2635bfc711d4dd1
brendanglancy <bglance68@gmail.com>
2023-12-06 17:57:14 -0500

    will work on this at skool

--- lib/neuralnettmp.h
+++ lib/neuralnettmp.h
@@ -43,27 +43,6 @@ inline float tanhf(float x) { return std::tanh(x); };
 float activationf(float x, Activation act);
 float activationdf(float x, Activation act);
 
-class Matrix {
- public:
-  Matrix(size_t rows, size_t cols);
-  void fill(float num);
-  void multiplication(Matrix dest, Matrix a, Matrix b);
-  void randomize(float low, float high);
-  void activation();
-  void print(const char* name, size_t padding) const;
-  void shuffle_rows(Matrix m);
-  void sigmoid(Matrix m);
-  void copy(Matrix dst, Matrix src);
-  void sum(const Matrix& a);
-
-  float& at(size_t i, size_t j);
-  const float& at(size_t i, size_t j) const;
-
- private:
-  size_t rows, cols;
-  std::vector<float> elements;
-};
-
 class Row {
  public:
   explicit Row(size_t cols);
